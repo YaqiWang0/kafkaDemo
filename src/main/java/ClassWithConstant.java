751a58676b411afc9ad1fb93bae1576f5b94ec38
yaqiwang <yaqiwang@ebay.com>
2019-07-04 13:53:44 +0800

    new

---
+++ src/main/java/ClassWithConstant.java
@@ -0,0 +1,15 @@
+
+
+    public class ClassWithConstant {
+
+        private static final Integer ANSWER = 42;
+
+
+        public static final int getAnswer() {
+
+            return ANSWER;
+
+        }
+
+    }
+
---
+++ src/main/java/ClassWithConstantTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+public class ClassWithConstantTest {
+
+    @org.junit.Test
+    public void getAnswer() {
+    }
+}
